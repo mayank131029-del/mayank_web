b1adab5b2f43ff4487f06091932dc86c683b7dd2
mayank131029-del <mayank131029@gmail.com>
2026-05-10 17:27:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ button.addEventListener("click", toggleMenu);
 
 let viewportWidth = window.innerWidth;
 console.log(viewportWidth);
+
+
+const back2top = document.querySelector(".back2top")
+
+window.addEventListener("scroll", ()=>{
+  if (window.scrollY>300) {
+    back2top.classList.add("show")
+  }
+  else{
+    back2top.classList.remove("show")
+  }
+})
